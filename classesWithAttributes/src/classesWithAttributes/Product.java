3315434e4826848044beaad67e1ed51a2400f194
Tuncay <Tuncay@LAPTOP-FVC34H3H>
2022-09-28 23:22:15 +0300

    First

--- classesWithAttributes/src/classesWithAttributes/Product.java
+++ classesWithAttributes/src/classesWithAttributes/Product.java
@@ -1,13 +1,73 @@
 package classesWithAttributes;
 
 public class Product {
-	
-	//attribute | field
-	int id;
-	String name;
-	String description;
-	double price;
-	int stockAmount;
-	 
 
+	// attribute | field
+	private int id;
+	private String name;
+	private String description;
+	private double price;
+	private int stockAmount;
+	private String renk;
+	private String kod;
+
+// getId komutu sadece oku anlamına geldiği için burada int komutu kullanılır.	
+	//getter
+	public int getId() {
+		return id;
+	}
+
+// Burada değeri set etmek istediğimiz için void kullanılır.
+
+	//setter		 Buradaki(Aşağıdaki) id, private int id; değildir.
+	public void setId(int id) {
+		this.id=id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getStockAmount() {
+		return stockAmount;
+	}
+
+	public void setStockAmount(int stockAmount) {
+		this.stockAmount = stockAmount;
+	}
+
+	public String getRenk() {
+		return renk;
+	}
+
+	public void setRenk(String renk) {
+		this.renk = renk;
+	}
+
+	public String getKod() {
+		return this.name.substring(0, 1) + id;
+	}
+
+
+	
 }
--- classesWithAttributes/src/classesWithAttributes/ProductManager.java
+++ classesWithAttributes/src/classesWithAttributes/ProductManager.java
@@ -2,8 +2,12 @@ package classesWithAttributes;
 
 public class ProductManager {
 	public void Add(Product product) {
-		//JDBC
-		System.out.println("Ürün eklendi " + product.name);
+		// JDBC
+		System.out.println("Ürün eklendi " + product.getName());
 	}
 
-} 
+	public void Add2(int id, String name, String description, int stockAmount, double price) {
+
+	}
+
+}
